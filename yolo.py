3baa85a75d185525340db8b701c87e73a2de0bfb
k.nakamu <kouki@kouki.local>
2019-05-30 01:27:19 +0900

    detect image test

--- yolo.py
+++ yolo.py
@@ -210,3 +210,23 @@ def detect_video(yolo, video_path, output_path=""):
             break
     yolo.close_session()
 
+def detect_img(yolo):
+    while True:
+        img = input('Input image filename:')
+        try:
+            image = Image.open(img)
+        except:
+            print('Open Error! Try again!')
+            continue
+        else:
+            r_image = yolo.detect_image(image)
+            print(type(r_image))
+            import cv2
+            cv2.imwrite("out.jpg", np.asarray(r_image)[..., ::-1])
+            #r_image.show()
+    yolo.close_session()
+
+
+
+if __name__ == '__main__':
+    detect_img(YOLO())
